3a66c0a8e20d88fed290789578c595f6d119837e
Blake Griffith <blake.a.griffith@gmail.com>
2013-05-09 14:31:39 -0500

    Initial commit of bool tests.

---
+++ test_bool.py
@@ -0,0 +1,14 @@
+""" Tests for the bool dtype sparse matrices, and boolean operations on
+sparse matrices.
+"""
+
+import numpy as np
+import scipy.sparse as sp
+
+def test_bool_operations():
+    """ Test that boolean operation beahave as expected."""
+    pass
+
+def test_bool_dtype():
+    """ Test that everything works with dtype=bool."""
+    pass
